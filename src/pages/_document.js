be0d59496bc7d8362b4aafa2ec8bcf175b5ff9e5
Abubakar Iliyasu <abubakarilyasu111@gmail.com>
2025-08-13 07:06:19 +1200

    Fix _document for vercel build

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,18 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
+    <Html lang="en">
+      <Head>
+        {/* Example: Adding an external JS library asynchronously */}
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+          strategy="beforeInteractive"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
-        {/* Bootstrap JS */}
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+5qBT3qxIJ6R3e1oz6tftG1LsJ5Uw"
-          crossOrigin="anonymous"
-        ></script>
       </body>
     </Html>
   );
